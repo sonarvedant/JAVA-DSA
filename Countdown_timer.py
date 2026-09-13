1cd4a0bd3e592cd68e907362c50e3925346bcc17
sonarvedant <sonarvedant22@gmail.com>
2026-09-13 12:16:13 +0530

    Update Countdown_timer.py

--- Countdown_timer.py
+++ Countdown_timer.py
@@ -3,6 +3,11 @@ import time
 #print("timeup")
 #then timeup is printed after 3 seconds
 
-my_timer = int(input("Enter the countdown time in seconds: "))
-for x in range(0, my_timer):
-    )
+my_timer = int(input("Enter the time in seconds: "))
+for x in reversed(range(0, my_timer)):
+    seconds = x % 60
+    minutes = int(x / 60)
+    hour = int(x / 3600)
+    print(f"{hour:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+print("Time's up!")
